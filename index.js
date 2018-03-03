e18757448390bf19df831ac4e6ed90bb59a79e28
yhuang133 <yhuang13@mail.uoguelph.ca>
2018-03-03 15:39:48 -0500

    first working version

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-stuffa
-a
+/**
+ * Module dependencies.
+ */
+'use strict';
+const port = process.env.PORT || 8000;
+const express = require('express');
+
+var app = express(); //module.exports = express.createServer();
+
+app.listen(port, () => console.log(`port is: ${port}!`));
+
+app.get('/', (req, res) => {
+    res.send(`Hey!`);
+});
