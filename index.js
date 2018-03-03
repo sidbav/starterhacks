7fbe4f56938dbb4ed15e6a6b206692a8baded3ae
yhuang133 <yhuang13@mail.uoguelph.ca>
2018-03-03 18:22:35 -0500

    quicksave

--- index.js
+++ index.js
@@ -4,11 +4,15 @@
 'use strict';
 const port = process.env.PORT || 8000;
 const express = require('express');
+const request = require('request');
+const gmail = require('./gmail'); //get the gmail.js stuff from this dir
 
 var app = express(); //module.exports = express.createServer();
 
 app.listen(port, () => console.log(`port is: ${port}!`));
 
 app.get('/', (req, res) => {
-    res.send(`Hey!`);
+    gmail.authorize().then((oauth2)=>{
+        res.send(gmail.listLabels(oauth2));
+    });
 });
